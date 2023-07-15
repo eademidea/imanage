e7dce99c4bedf56601e42197b3ec3e6cfb4428e6
conrado <conradocjo@gmail.com>
2023-07-14 21:35:20 -0300

    Evolução de código

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -7,8 +7,9 @@ import { Request, Response } from 'express';
 @Controller("v1/user")
 export class UserController {
 
-    @Get('test')
+    @Get('login')
     public get(req: Request, res: Response) {
+        console.log(req.query)
         console.log("Cheguei aqui")
         return res.json({
             message: 'get_called',
