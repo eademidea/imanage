dd4fb9a8fce93d9bba0898992e25a8d2fa6ac2cf
conrado <conrado@localhost.localdomain>
2023-07-17 19:02:48 -0300

    Validações

--- src/validations/UserValidator.ts
+++ src/validations/UserValidator.ts
@@ -7,8 +7,8 @@ interface IBodyProps extends Omit<IUser, "id" | "created_at"> { }
 
 export default validation((getSchema) => ({
     body: getSchema<IBodyProps>(yup.object().shape({
-        name: yup.string().required().min(3).max(150),
-        username: yup.string().required().min(3).max(150),
-        password: yup.string().required().min(3).max(150)
+        name: yup.string().required().min(8).max(150),
+        username: yup.string().required().min(5).max(150),
+        password: yup.string().required().min(8).max(30)
     })),
 }));
